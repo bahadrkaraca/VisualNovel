391654327b854506465c3659d4db571411aaa47f
BahadrK <62482313+Bahadr6161@users.noreply.github.com>
2024-07-04 21:29:28 +0300

    Starting Conversations Video 7

--- Assets/_MAİN/Scripts/Core/Dialogue/DialogueContainer.cs
+++ Assets/_MAİN/Scripts/Core/Dialogue/DialogueContainer.cs
@@ -8,7 +8,7 @@ namespace DIALOGUE
     public class DialogueContainer
     {
         public GameObject root;
-        public TextMeshProUGUI nameText;
+        public NameContainer nameContainer = new NameContainer();
         public TextMeshProUGUI dialogueText;
     }
 }
