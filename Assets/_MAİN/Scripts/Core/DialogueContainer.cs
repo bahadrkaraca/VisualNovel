5db7a0a36930851e371115e20c2859aca83a14d6
BahadrK <62482313+Bahadr6161@users.noreply.github.com>
2024-07-04 19:06:31 +0300

    Dialogue Parsing Video 6

--- Assets/_MAİN/Scripts/Core/DialogueContainer.cs
+++ Assets/_MAİN/Scripts/Core/DialogueContainer.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
-[System.Serializable]
-public class DialogueContainer
+namespace DIALOGUE
 {
-    public GameObject root;
-    public TextMeshProUGUI nameText;
-    public TextMeshProUGUI dialogueText;
+    [System.Serializable]
+    public class DialogueContainer
+    {
+        public GameObject root;
+        public TextMeshProUGUI nameText;
+        public TextMeshProUGUI dialogueText;
+    }
 }
--- Assets/_MAİN/Scripts/Core/DialogueSystem.cs
+++ Assets/_MAİN/Scripts/Core/DialogueSystem.cs
@@ -1,31 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class DialogueSystem : MonoBehaviour
+namespace DIALOGUE
 {
-    public DialogueContainer dialogueContainer = new DialogueContainer();
-
-    public static DialogueSystem instance;
-
-    private void Awake()
+    public class DialogueSystem : MonoBehaviour
     {
-        if (instance == null)
-            instance = this;
-        else
-            DestroyImmediate(gameObject);
-    }
+        public DialogueContainer dialogueContainer = new DialogueContainer();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        public static DialogueSystem instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else
+                DestroyImmediate(gameObject);
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
     }
 }
